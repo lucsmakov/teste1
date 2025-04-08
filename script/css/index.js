de911cd7eb3fac279579707fd4c415112173c10d
nome <oclassicvanilla@gmail.com>
2025-04-08 04:18:59 -0300

    third

--- script/css/index.js
+++ script/css/index.js
@@ -1,18 +1,33 @@
 function showSideContent() {
-      const side = document.getElementById("side-content");
-      side.classList.toggle("show");
+    const side = document.getElementById("side-content");
+    side.classList.toggle("show");
 }
 function KeyFrameLoad() {
-    document.addEventListener("DOMContentLoaded", () => {
     const container = document.getElementById("container-keyframe");
-        setTimeout(() => {
-            if (container) {
+    setTimeout(() => {
+        if (container) {
             container.style.opacity = "0";
             setTimeout(() => {
-            container.style.display = "none";
+                container.style.display = "none";
             }, 1000);
+        }
+    }, 1000);
+    const imagens = document.querySelectorAll('.fade');
+
+    const observador = new IntersectionObserver(function(entradas) {
+        entradas.forEach(function(entrada) {
+            if (entrada.isIntersecting) {
+                entrada.target.classList.add('visible');
+            } else {
+                entrada.target.classList.remove('visible');
             }
-        }, 1000);
+        });
+    }, { threshold: 0.6 });
+
+    imagens.forEach(function(imagem) {
+        observador.observe(imagem);
     });
 }
-KeyFrameLoad();
+document.addEventListener("DOMContentLoaded", () => {
+    KeyFrameLoad();
+});
